ab300683a57a64f5c04491675f0b3fc2ed4621bd
Trent Robbins <robbintt@gmail.com>
2015-11-26 04:32:22 -0500

    Added updated example config.

--- smtp_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# TAR 040915
-# From: http://stackoverflow.com/questions/5802189/django-errno-111-connection-refused
-# There is a django alternative listed in this answer that may be
-# better in the long term. We should explore that too. (Django: console.EmailBackend)
-python -m smtpd -n -c DebuggingServer localhost:1025
